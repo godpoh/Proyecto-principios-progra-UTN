296ae4708b03055edeba7de39183e2e99e18b51f
godpoh <120140501+godpoh@users.noreply.github.com>
2024-02-29 14:48:50 -0600

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+ventana_madre = Tk()
+
+# Cambiar el color de fondo a rojo
+ventana_madre.configure(bg="grey")
+
+ventana_madre.title("GUI")
+
+ventana_madre.geometry("650x350")
+
+ventana_madre.mainloop()
+
+miframe = Frame
